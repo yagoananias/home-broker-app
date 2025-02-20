0c85cbc99a5a6675b73d72f1da8da8ad1981ebaf
Yago Ananias <yago.crazy@hotmail.com>
2025-02-19 23:48:05 -0300

    :sparkles: feat: add Asset entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AssetsModule } from './assets/assets.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AssetsModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://root:root@localhost:27017/nest?authSource=admin&directConnection=true'), AssetsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/assets/entities/asset.entity.ts
+++ src/assets/entities/asset.entity.ts
@@ -1,1 +1,35 @@
-export class Asset {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+import crypto from 'crypto';
+
+export type AssetDocument = HydratedDocument<Asset>;
+
+@Schema({
+  timestamps: true,
+  collectionOptions: {
+    changeStreamPreAndPostImages: {
+      enabled: true,
+    },
+  },
+})
+export class Asset {
+  @Prop({ default: () => crypto.randomUUID() })
+  _id: string;
+
+  @Prop({ unique: true, index: true })
+  name: string;
+
+  @Prop({ unique: true, index: true })
+  symbol: string;
+
+  @Prop()
+  image: string;
+
+  @Prop()
+  price: number;
+
+  createdAt!: Date;
+  updatedAt!: Date;
+}
+
+export const AssetSchema = SchemaFactory.createForClass(Asset);
